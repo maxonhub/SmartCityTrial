3bf23a012b876675536b21313dd6392efa94ce27
moberlis <moberlis@FR-33-02-13-035.corp.capgemini.com>
2016-08-23 11:44:46 +0200

    Filtering and sorting words in a file

--- Test/src/MyWordCounts.py
+++ Test/src/MyWordCounts.py
@@ -9,7 +9,16 @@ from gc import collect
  
 # The 'os' library allows us to read the environment variable SPARK_HOME defined in the IDE environment
 import os
- 
+import re 
+
+def MapCorrectWords( word ):
+        res = (False, 0)
+        
+        if len(re.findall(".", word, 0)) > 1 :  
+            res = (True, word)
+              
+        return res
+
 # Configure the Spark context to give a name to the application
 sparkConf = SparkConf().setAppName("MyWordCounts")
 sc = SparkContext(conf = sparkConf)
@@ -19,9 +28,15 @@ textFile = sc.textFile(os.environ["SPARK_HOME"] + "\README.md")
 
 # The code for counting the words (note that the execution mode is lazy)
 # Uses the same paradigm Map and Reduce of Hadoop, but fully in memory
-wordCounts = textFile.flatMap(lambda line: line.split()) \
+wordCounts = textFile.flatMap(lambda line: line.split(" ")) \
+    .map(lambda word: MapCorrectWords(word)) \
     .map(lambda word: (word, 1)) \
-    .reduceByKey(lambda a, b: a+b)
- 
+    .reduceByKey(lambda a, b: a+b) \
+    .map(lambda x : (x[1], x[0])) \
+    .sortByKey() \
+    .map(lambda x : (x[1], x[0]))
+   
 # Executes the DAG (Directed Acyclic Graph) for counting and collecting the result
-for wc in wordCounts.collect(): print(wc)
+print()
+for wc in wordCounts.collect(): print(wc[0][1], " occured", wc[1])
+print()
